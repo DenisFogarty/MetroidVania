3ecc0db3934bf00da3b4fe864a85d987428233e8
Denis Fogarty <fogartydenis1@gmail.com>
2015-01-13 20:12:48 +0000

    Craeted thread to calculate bullet position, need to create method to calculate position and call this from thread

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ const int SCREEN_W = 600;
 const int SCREEN_H = 480;
 const int PLAYER_SIZE = 32;
 
-static void *refresh_thread(ALLEGRO_THREAD *thread, void *arg);
+static void *shoot(ALLEGRO_THREAD *thread, void *arg);
 
 
 int main() {
 	ALLEGRO_EVENT_QUEUE *event_queue    = NULL;
 	ALLEGRO_TIMER       *timer		    = NULL;
-	ALLEGRO_THREAD		*refresh_1		= NULL;
+	ALLEGRO_THREAD		*shoot_thread	= NULL;
 
 	if(!al_init()) {
 		fprintf(stderr, "Failed to initialise Allegro");
@@ -57,7 +57,53 @@ int main() {
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_mouse_event_source());
 
-	al_rest(5);
+
+	draw_display draw;
+	SHOOT shoot_data;
+	SHOOT2 shoot_data2;
+
+
+
+	shoot_thread = al_create_thread(shoot, &shoot_data);
+	al_start_thread(shoot_thread);
+
+	al_lock_mutex(shoot_data.mutex);
+	while(!shoot_data.READY) {
+		al_wait_cond(shoot_data.cond, shoot_data.mutex);
+	}
+	al_unlock_mutex(shoot_data.mutex);
+
+
+
+	al_set_target_bitmap(al_get_backbuffer(draw.display));
+
+	int i = 0;
+
+	while(i < 300) {
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+
+		al_lock_mutex(shoot_data.mutex);
+
+		al_draw_line(shoot_data.x1, shoot_data.y1, shoot_data.x2, shoot_data.y2, al_map_rgb(255, 0, 0), 3);
+
+		al_unlock_mutex(shoot_data.mutex);
+
+
+		al_lock_mutex(shoot_data2.mutex);
+
+		al_draw_line(shoot_data2.x1, shoot_data2.y1, shoot_data2.x2, shoot_data2.y2, al_map_rgb(0, 255, 0), 3);
+
+		al_unlock_mutex(shoot_data2.mutex);
+
+
+		al_flip_display();
+
+		i++;
+
+		al_rest(1.0/60.0);
+	}
+
+	al_destroy_thread(shoot_thread);
 }
 
 
@@ -67,6 +113,26 @@ draw_display::draw_display() {
 }
 
 
-static void *refresh_thread(ALLEGRO_THREAD *thread, void *arg) {
+static void *shoot(ALLEGRO_THREAD *thread, void *arg) {
+	SHOOT *shoot_data = (SHOOT*) arg;
 
+	al_lock_mutex(shoot_data->mutex);
+	shoot_data->READY = true;
+	al_broadcast_cond(shoot_data->cond);
+	al_unlock_mutex(shoot_data->mutex);
+
+	while(!al_get_thread_should_stop(thread)) {
+		al_lock_mutex(shoot_data->mutex);
+
+		shoot_data->x1 += 1;
+		shoot_data->y1 += 1;
+		shoot_data->x2 += 1;
+		shoot_data->y2 += 1;
+
+		al_unlock_mutex(shoot_data->mutex);
+
+		std::cout << shoot_data->x1 << std::endl;
+
+		al_rest(1.0/120.0);
+	}
 }
--- main.h
+++ main.h
@@ -10,16 +10,52 @@
 class draw_display {
 public:
 	draw_display();
-private:
 	ALLEGRO_DISPLAY *display;
 	ALLEGRO_BITMAP *foreground;
+
+private:
 
 protected:
 
 };
 
-class refresh {
+class SHOOT {
 public:
+	int x1, y1, x2, y2;
+	ALLEGRO_MUTEX *mutex;
+	ALLEGRO_COND *cond;
+	bool READY;
+	SHOOT():
+		x1(0),
+		y1(0),
+		x2(10),
+		y2(10),
+		mutex(al_create_mutex()),
+		cond(al_create_cond()),
+		READY(false)
+	{}
+
+private:
+
+protected:
+
+};
+
+class SHOOT2 {
+public:
+	int x1, y1, x2, y2;
+	ALLEGRO_MUTEX *mutex;
+	ALLEGRO_COND *cond;
+	bool READY;
+	SHOOT2():
+		x1(640),
+		y1(0),
+		x2(630),
+		y2(10),
+		mutex(al_create_mutex()),
+		cond(al_create_cond()),
+		READY(false)
+	{}
 
 private:
 
