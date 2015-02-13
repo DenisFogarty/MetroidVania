b9941fc03a8f80550d4f47afbfff2590a5a86106
Denis Fogarty <fogartydenis1@gmail.com>
2015-02-13 16:40:19 +0000

    items class added

---
+++ items.cpp
@@ -0,0 +1,18 @@
+/*
+ * items.cpp
+ *
+ *  Created on: 13 Feb 2015
+ *      Author: denis2
+ */
+
+#include "items.h"
+
+items::items() {
+	// TODO Auto-generated constructor stub
+
+}
+
+items::~items() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ items.h
@@ -0,0 +1,17 @@
+/*
+ * items.h
+ *
+ *  Created on: 13 Feb 2015
+ *      Author: denis2
+ */
+
+#ifndef ITEMS_H_
+#define ITEMS_H_
+
+class items {
+public:
+	items();
+	virtual ~items();
+};
+
+#endif /* ITEMS_H_ */
